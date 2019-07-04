3a0d6d3835e42618438dbd8e450c2858fa99b3da
a.gamal <a.gamal@cequens.com>
2019-07-04 15:55:17 +0200

    editing config gile

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Copyright 2016-2018 Apple, Inc.
 # All Rights Reserved.
 
-BUSINESS_CHAT_SERVER = "https://mspgw.push.apple.com/v1"
+BUSINESS_CHAT_SERVER = "https://mspgw-int.push.apple.com/v1"
 
-CSP_ID = "1ee033f3-5b84-11e7-a0b6-c753b1bff6fa"
-BIZ_ID = "aef8d111-5b9b-11e7-a0b6-c753b1bff6fa"
+CSP_ID = "c242a3c7-a0e3-4688-9a03-fc3ecd18f409"
+BIZ_ID = "e937376b-2bd4-4e33-bade-edaa9fab0ef4"
 
-SECRET = "bNUK+JrMVGDmoy9kilTgHvXUfyRePmaDWxCHVVjJqUE="
+SECRET = "yjq7zIrBBlIas7BBIJfbep8aLnvc6r8gEjvOxXHesew="
 
 IMESSAGE_EXTENSION_BID = "com.apple.messages.MSMessageExtensionBalloonPlugin:0000000000:com.apple.icloud.apps.messages.business.extension"
