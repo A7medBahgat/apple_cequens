11f7b9b07c092615fb9c5d87cad3b51a7de76896
a.gamal <ahmadfoda92@gmail.com>
2019-07-04 20:12:57 +0200

    aasd

--- attachment_cipher.py
+++ attachment_cipher.py
@@ -5,7 +5,7 @@ import os
 import base64
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
-import codecs
+import binascii
 
 def encrypt(data):
     key = os.urandom(32)
@@ -32,8 +32,8 @@ def decrypt(encrypted_data, orig_key):
         iv += chr(i)
 
     print('iv ',iv);
-    # ctr = Counter.new(128, initial_value=int(iv.encode("hex"), 16))
-    ctr = Counter.new(128, initial_value=int(codecs.encode(iv, "hex"), 16))
+    ctr = Counter.new(128, initial_value=int(binascii.hexlify(iv), 16))
+    # ctr = Counter.new(128, initial_value=int(codecs.encode(iv, "hex"), 16))
     # codecs.decode('1deadbeef4', 'hex')
     cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
     decrypted = cipher.decrypt(encrypted_data)
