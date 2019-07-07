a3e2daaa5e37affb0af08e4a0eb51ef49e49fd4c
Ahmedbahgat <1400044@eng.asu.edu.eg>
2019-07-07 18:14:20 +0200

    modify

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, request, abort
+
 app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+from .step3_5_large_interactive_message import app as step3_5
+from .step3_5_large_interactive_message import receive_large_interactive_payload
+
+# exec(open('step3_5_large_interactive_message.py').read())
+# from .step3_5_large_interactive_message import receive_large_interactive_payload
+app.route("/message", methods=['POST'])(receive_large_interactive_payload)
+
+app.run(host='127.0.0.1', port=8002)
 
 # def landingPage():
 #     return render_template("landingPage.html")
