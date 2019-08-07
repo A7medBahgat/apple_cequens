1b10f2d87975c96db117a80f1d0ddf8c8908c35c
a.gamal <ahmadfoda92@gmail.com>
2019-08-07 11:34:10 +0200

    adding app.py attachment edit

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ def home():
 
 from step3_5_large_interactive_message import app as step3_5
 from step3_5_large_interactive_message import receive_large_interactive_payload
+from send_message_with_image_attachment import send_message_with_image_attachment
 
 # exec(open('step3_5_large_interactive_message.py').read())
 # from .step3_5_large_interactive_message import receive_large_interactive_payload
 app.route("/message", methods=['POST'])(receive_large_interactive_payload)
+app.route("/sendattachment", methods=['POST'])(send_message_with_image_attachment)
 
 # app.run(host='127.0.0.1', port=8002)
 
